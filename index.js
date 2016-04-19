06c0101c992bd056724831bbed6bb1f1aea1ad84
Martin Imfeld <martin.imfeld@gmail.com>
2016-04-19 17:08:29 +0200

    getting list of src files in root folder of repo

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 
 const bitbucket = require(__dirname + '/lib/bitbucket')
 
+/*
 bitbucket.demo()
   .then(data => { console.log(data) })
   .catch(err => { console.error(err) })
 
 bitbucket.changesets('martianfield', 'bindery_test')
+  .then(data => { console.log(data) })
+  .catch(err => { console.error(err) })
+*/
+
+bitbucket.files('martianfield', 'bindery_test')
   .then(data => { console.log(data) })
   .catch(err => { console.error(err) })
