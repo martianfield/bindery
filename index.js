0a6b3edf8e0c2379f25189157055d26f1905c291
Martin Imfeld <martin.imfeld@gmail.com>
2016-04-24 07:08:58 +0200

    getting raw source

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 
 const bitbucket = require(__dirname + '/lib/bitbucket')
 
-/*
-bitbucket.demo()
-  .then(data => { console.log(data) })
-  .catch(err => { console.error(err) })
 
-bitbucket.changesets('martianfield', 'bindery_test')
-  .then(data => { console.log(data) })
-  .catch(err => { console.error(err) })
-*/
+// bitbucket.demo()
+//   .then(data => { console.log(data) })
+//   .catch(err => { console.error(err) })
 
-bitbucket.files('martianfield', 'bindery_test')
-  .then(data => { console.log("done:", data) })
-  .catch(err => { console.error(err) })
+// bitbucket.changesets('martianfield', 'bindery_test')
+//   .then(data => { console.log(data) })
+//   .catch(err => { console.error(err) })
+
+
+// bitbucket.files('martianfield', 'bindery_test')
+//   .then(data => { console.log("done:", data) })
+//   .catch(err => { console.error(err) })
+
+bitbucket.file('martianfield', 'bindery_test', '91c6b8d9690b7629e9aea966df3456195318aaaa', 'sub1/test_sub_1.md')
+  .then(data => { console.log("src:", data)})
+  .catch(err => { console.error(err)})
