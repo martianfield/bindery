cd23f25074edcf9727a24092160de94117ffc63d
Martin Imfeld <martin.imfeld@gmail.com>
2016-04-24 07:40:51 +0200

    added ticks to fileinfo

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ const bitbucket = require(__dirname + '/lib/bitbucket')
 //   .catch(err => { console.error(err) })
 
 
-// bitbucket.files('martianfield', 'bindery_test')
-//   .then(data => { console.log("done:", data) })
-//   .catch(err => { console.error(err) })
+bitbucket.files('martianfield', 'bindery_test')
+  .then(data => { console.log("done:", data) })
+  .catch(err => { console.error(err) })
 
-bitbucket.file('martianfield', 'bindery_test', '91c6b8d9690b7629e9aea966df3456195318aaaa', 'sub1/test_sub_1.md')
-  .then(data => { console.log("src:", data)})
-  .catch(err => { console.error(err)})
+// bitbucket.file('martianfield', 'bindery_test', 'dc804605bb17', 'sub1/test_sub_2.md')
+//   .then(data => { console.log("src:", data)})
+//   .catch(err => { console.error(err)})
